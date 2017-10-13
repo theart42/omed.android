ce6f61e5e69a02f17118fb1fe5d4ffd924b32903
Frank Spierings <frank.spierings@gmail.com>
2017-10-13 15:00:04 +0200

    Cleaned up the IO code and added the actual resource.

--- app/src/main/java/nl/secureapps/demohacklu/IoDemo.java
+++ app/src/main/java/nl/secureapps/demohacklu/IoDemo.java
@@ -1,18 +1,8 @@
 package nl.secureapps.demohacklu;
 
-import android.content.Intent;
-import android.content.res.AssetManager;
-import android.support.v4.content.LocalBroadcastManager;
-
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.StringReader;
-
-/**
- * Created by matth on 28/09/2017.
- */
 
 public class IoDemo {
     public void startIoDemo() {
